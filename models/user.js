2a64804e22951c65d637384503c5385f786881ec
Ankit-Bhatt1 <bhattankit515@gmail.com>
2024-05-28 16:21:47 +0530

    Forgot Password Added

--- models/user.js
+++ models/user.js
@@ -1,3 +1,24 @@
+// const mongoose = require('mongoose');
+
+// const userSchema = new mongoose.Schema({
+//     username: {
+//         type: String,
+//         required: [true, 'Username cannot be blank']
+//     },
+//     email: {
+//         type: String,
+//         required: [true, 'Email cannot be blank'],
+//         unique: true // Ensure email is unique
+//     },
+//     password: {
+//         type: String,
+//         required: [true, 'Password cannot be blank']
+//     }
+// });
+
+// const User = mongoose.model('User', userSchema);
+
+// module.exports = User;
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
@@ -13,6 +34,14 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: [true, 'Password cannot be blank']
+    },
+    otp: {
+        type: Number, // Assuming OTP is stored as a number
+        default: null // Default value for OTP
+    },
+    otpExpiration: {
+        type: Date, // Assuming OTP expiration is stored as a Date
+        default: null // Default value for OTP expiration
     }
 });
 
